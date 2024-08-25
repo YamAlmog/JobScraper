5c2c40e448e6514a186d0662e8fedb62062d3565
YamAlmog <yamalmog14@gmail.com>
2024-08-25 19:01:28 +0300

    complete google scraper for now

--- google_job_scraper.py
+++ google_job_scraper.py
@@ -25,68 +25,62 @@ class GoogleScraper(BaseScraper):
     def __init__(self) -> None:    
         super().__init__(driver_path)
 
+
     def retriev_positions(self, url, title_to_search, location):
         try:
+            self.driver.maximize_window()
             self.driver.get(url)
             
             # Wait until the title field is present
             title_search_field_elem = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.ID, TITLE_FIELD_ID))
-            )
+                EC.presence_of_element_located((By.ID, TITLE_FIELD_ID)))
             title_search_field_elem.send_keys(title_to_search)
-            
+            time.sleep(1)
+
             # Wait until the location field is present
             location_search_field_elem = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.ID, LOCATION_FIELD_ID))
-            )
+                EC.presence_of_element_located((By.ID, LOCATION_FIELD_ID)))
             location_search_field_elem.send_keys(location)
-
-            time.sleep(1)
             
             WebDriverWait(self.driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '//*[@id="c10"]/span[3]'))
-            ).click()
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="c10"]/span[3]'))).click()
 
             # Click the search button
             click_search_field_elem = WebDriverWait(self.driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, CLICK_SEARCH_BUTTON))
-            )
-
-
+                EC.element_to_be_clickable((By.XPATH, CLICK_SEARCH_BUTTON)))
             click_search_field_elem.click()
 
             time.sleep(1)
 
-            # Retrieve all positions
-            list_of_positions = []
-            # all_positions = WebDriverWait(self.driver, 10).until(
-            #     EC.presence_of_all_elements_located((By.XPATH, POSITIONS_XPATH))
-            # )
-            all_positions_element = self.driver.find_element(By.XPATH, POSITIONS_XPATH)
-            positions = all_positions_element.find_elements(By.XPATH, './*')
-
-            for position in positions:
-
-                # print(f"-------------------------------\n {position.text} \n-------------------------------")
-                print('Location: ' + position.find_element(By.CLASS_NAME, 'r0wTof ').text)
-                continue
-                
-                title_elem = position.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[1]/div/div[2]/div/div/div[2]/main/div/c-wiz/div/ul/li[3]/div/div/div[1]/div/div[1]/div')
-                pos_title = title_elem.text
-                print(f"position title: {pos_title}")
-                
-                location_elem = position.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[1]/div/div[2]/div/div/div[2]/main/div/c-wiz/div/ul/li[3]/div/div/div[1]/div/div[3]/p/span')
-                pos_location = location_elem.text
-                print(f"position location: {pos_location}")
-                
-                position = Position(title=pos_title, location=pos_location)
-                list_of_positions.append(position)
-            return list_of_positions
+            # return all positions list
+            return self.create_positions_list()
 
         except Exception as e:
             print(f"An error occurred: {e}")
 
 
+    def create_positions_list(self):
+        list_of_positions = []
+        all_positions_element = self.driver.find_element(By.XPATH, POSITIONS_XPATH)
+        positions = all_positions_element.find_elements(By.XPATH, './*')
+
+        for position in positions:
+            title_elem = position.find_element(By.CLASS_NAME, 'QJPWVe')
+            pos_title = title_elem.text
+           
+            location_elem = position.find_element(By.CLASS_NAME, 'r0wTof ')
+            pos_location = location_elem.text
+            
+            link_element = position.find_element(By.CLASS_NAME, 'WpHeLc')
+            job_link = link_element.get_attribute('href')
+        
+            description_element = position.find_element(By.CLASS_NAME, 'Xsxa1e')
+            pos_description = description_element.text
+            
+            position = Position(title=pos_title, location=pos_location, link=job_link, description=pos_description)
+            list_of_positions.append(position)
+        
+        return list_of_positions    
 
 google_scraper = GoogleScraper()
-google_scraper.retriev_positions(CAREERS_URL, 'software engineer', 'Israel')
+print(google_scraper.retriev_positions(CAREERS_URL, 'Junior Software Engineer', 'Israel'))
